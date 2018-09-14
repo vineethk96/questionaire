40c18891192fcd1899962e325f76cba5423923d8
Mohammad Aarij <moha96@vt.edu>
2018-09-14 13:43:04 -0400

    Update BridgeKeys.py with credentials

--- BridgeKeys.py
+++ BridgeKeys.py
@@ -1,3 +1,3 @@
-watson_username = ''
-watson_password = ''
-watson_url = ''
+watson_username = '81a63a25-ea40-4a64-8ada-f3d94bc38cd6'
+watson_password = 'https://stream.watsonplatform.net/text-to-speech/api'
+watson_url = 'https://stream.watsonplatform.net/text-to-speech/api'
