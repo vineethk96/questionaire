11d2800fa9ab3945902ef7d351f7600d7345e379
Vineeth Kirandumkara <vineethk@vt.edu>
2018-09-14 14:28:42 -0400

    Update ClientKeys.py

--- ClientKeys.py
+++ ClientKeys.py
@@ -1,4 +1,4 @@
-consumer_key = ''
-consumer_secret = ''
+consumer_key = 'UZiXERP0M0OkVG3hKvRucimSn'
+consumer_secret = 'aHzvfL2lbZzmDhHx11qBZzJqQwB0BuwUFxVYJnp7S3RRsW0CH5'
 access_token = ''
 access_token_secret = ''
