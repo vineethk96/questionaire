00304fbee99ee07b149b9ba887810a454bf1f401
Mohammad Aarij <moha96@vt.edu>
2018-09-19 22:04:16 -0400

    Update credentials with IAM tokens

--- BridgeKeys.py
+++ BridgeKeys.py
@@ -1,3 +1,7 @@
-watson_username = '81a63a25-ea40-4a64-8ada-f3d94bc38cd6'
-watson_password = 'https://stream.watsonplatform.net/text-to-speech/api'
-watson_url = 'https://stream.watsonplatform.net/text-to-speech/api'
+#watson_username = '81a63a25-ea40-4a64-8ada-f3d94bc38cd6'
+#watson_password = 'https://stream.watsonplatform.net/text-to-speech/api'
+#watson_url = 'https://stream.watsonplatform.net/text-to-speech/api'
+
+watson_api_key = 'eQ9vCoecZd4ka7Xn-f07RqRyUHbKIAgLngt-MZlXRZ0_'
+watson_url = 'https://gateway-wdc.watsonplatform.net/text-to-speech/api'
+
